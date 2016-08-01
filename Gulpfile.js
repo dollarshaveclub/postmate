0cffe7455edc8d3b176ddad3421a8e6ef8a35c41
Jacob Kelley <jakie8@gmail.com>
2016-07-31 20:58:39 -0700

    Lint

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,30 +1,31 @@
 
-const babel   = require('rollup-plugin-babel');
-const eslint  = require('gulp-eslint');
-const gulp    = require('gulp');
-const minify  = require('uglify-js').minify;
-const rollup  = require('rollup-stream');
-const source  = require('vinyl-source-stream');
-const uglify  = require('rollup-plugin-uglify');
+const babel = require('rollup-plugin-babel');
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const minify = require('uglify-js').minify;
+const rollup = require('rollup-stream');
+const source = require('vinyl-source-stream');
+const uglify = require('rollup-plugin-uglify');
 
-gulp.task('build', () => rollup({
+gulp.task('build', () =>
+  rollup({
     entry: './lib/postmate.js',
     format: 'umd',
     moduleName: 'Postmate',
     plugins: [
       babel({
-        exclude: 'node_modules/**'
+        exclude: 'node_modules/**',
       }),
-      uglify({}, minify)
-    ]
+      uglify({}, minify),
+    ],
   })
-  .pipe(source('postmate.min.js'))
-  .pipe(gulp.dest('./build'))
+    .pipe(source('postmate.min.js'))
+    .pipe(gulp.dest('./build'))
 );
 
-gulp.task('lint', () => {
-    return gulp.src(['**/*.js','!node_modules/**', '!build/**'])
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
-});
+gulp.task('lint', () =>
+  gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
