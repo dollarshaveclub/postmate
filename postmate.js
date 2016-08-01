bc319c10d4ca3689dad75dbcacddda9cb7457e91
Jacob Kelley <jakie8@gmail.com>
2016-07-31 20:05:19 -0700

    Fixed handshake flow

--- postmate.js
+++ postmate.js
@@ -1,9 +1,10 @@
 ;(function () {
 
   const MESSAGE_TYPE = 'application/x-postmate-v1+json';
+  let debug = true;
 
   function log() {
-    // console.log.apply(console, arguments);
+    if (debug) console.log.apply(console, arguments);
   }
 
   /**
@@ -32,7 +33,6 @@
       return {
         frame,
         get: function (key) {
-          console.log("Getting", key);
           return new Promise(function (resolve, reject) {
 
             // Extract data from response and kill listeners
@@ -85,75 +85,65 @@
     this.context = context;
     this.client = this.host.parent;
 
-    log('Host: Sending handshake');
+    return new Promise((resolve, reject) => {
 
-    let prom = new Promise((resolve, reject) => {
-      log('Host: Adding handshake reply listener');
-
-      let reply = e => {
-        if( e.data.postmate === "handshake-reply") {
-          log('Host: Received handshake reply from Client');
-          log('Host: Removing handshake reply listener');
-          this.host.removeEventListener('message', reply, false);
+      let shake = e => {
+        if( e.data.postmate === "handshake") {
+          log('Host: Received handshake from Client');
+          this.host.removeEventListener('message', shake, false);
+          log('Host: Sending handshake reply to Client');
+          e.source.postMessage({
+            postmate: 'handshake-reply',
+            type: MESSAGE_TYPE
+          }, e.origin);
           return resolve(new Consumer(this));
         }
         return reject("Handshake Reply Failed");
       };
-      this.host.addEventListener('message', reply, false);
+      this.host.addEventListener('message', shake, false);
     });
-
-    this.client.postMessage({
-      postmate: 'handshake',
-      type: MESSAGE_TYPE
-    }, "*");
-    return prom;
   }
 
-  /**
-   * Establishes a 3-way handshake with the host (refactor for 2 way)
-   *
-   * The negotiation strategy is as follows:
-   * 1. Synchronize: The client awaits for a message from the Host. When the Host loads, it sends the Client the handshake.
-   * 2. Reply: The Client will receive the handshake and send back a reply.
-   * 3. Acknowledgement: Once the Host receives the reply, it sends one final message to let the client know events have been established.
-   *
-   * @param {DocumentElement} container The element to inject the frame information
-   * @param {String} url    The URL to load
-   * @param {String} url    The default context from the Client to extend in the Host
-   */
-  Postmate.Handshake = function (container, url, context) {
+
+  Postmate.Handshake = function (userOptions) {
+    let {container, url} = userOptions;
+
+    // if ('debug' in userOptions) {
+    //   debug = userOptions.debug;
+    // }
+
     this.id = 'client';
-    this.context = context;
     this.client = window;
     this.frame = document.createElement('iframe');
     container.appendChild(this.frame);
     this.host = this.frame.contentWindow;
 
-    var prom = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-      let shake = e => {
-        if (e.data.postmate === "handshake") {
-          log('Client: Received handshake from Host');
-          log('Client: Removing handshake listener');
-          this.client.removeEventListener('message', shake, false);
-          log('Client: Sending handshake reply');
-          e.source.postMessage({
-            postmate: "handshake-reply",
-            type: MESSAGE_TYPE
-          }, e.origin);
+      let reply = e => {
+        if (e.data.postmate === "handshake-reply") {
+          log('Client: Received handshake reply from Host');
+          this.client.removeEventListener('message', reply, false);
           return resolve(new Consumer(this));
         } else {
-          log('Client: Invalid handshake');
+          log('Client: Invalid handshake reply');
           return reject('Failed handshake');
         }
       }
 
-      log('Client: Adding handshake listener');
-      this.client.addEventListener('message', shake, false);
-    });
+      this.client.addEventListener('message', reply, false);
 
-    this.frame.src = url;
-    return prom;
+      this.frame.onload = () => {
+        log('Client: Sending handshake');
+        this.host.postMessage({
+          postmate: 'handshake',
+          type: MESSAGE_TYPE
+        }, "*");
+      }
+
+      log('Client: Loading frame');
+      this.frame.src = url;
+    });
   };
 
   window.Postmate = Postmate;
