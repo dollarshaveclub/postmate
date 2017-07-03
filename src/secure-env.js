739ea17c0bb648e51e99ee0d3f78c7a59ca06abc
Marat Dyatko <marat.dyatko@klarna.com>
2017-07-03 21:14:01 +0200

    Fail safely

--- src/secure-env.js
+++ src/secure-env.js
@@ -8,26 +8,48 @@ if (window) {
   if (Object.defineProperty) {
     ['crypto', 'msCrypto', 'Math'].forEach((prop) => {
       propertyDefinition.value = window[prop];
-      Object.defineProperty(window, prop, propertyDefinition);
+
+      try {
+        Object.defineProperty(window, prop, propertyDefinition);
+      } catch (e) {
+        console.error(e);
+      }
     });
 
     ['random', 'max', 'min', 'pow', 'floor', 'ceil'].forEach((prop) => {
       propertyDefinition.value = Math[prop];
-      Object.defineProperty(Math, prop, propertyDefinition);
+
+      try {
+        Object.defineProperty(Math, prop, propertyDefinition);
+      } catch (e) {
+        console.error(e);
+      }
     });
   }
 
   if (Object.freeze) {
     if (window.crypto) {
-      Object.freeze(window.crypto);
+      try {
+        Object.freeze(window.crypto);
+      } catch (e) {
+        console.error(e);
+      }
     }
 
     if (window.msCrypto) {
-      Object.freeze(window.msCrypto);
+      try {
+        Object.freeze(window.msCrypto);
+      } catch (e) {
+        console.error(e);
+      }
     }
 
     if (window.performance) {
-      Object.freeze(window.performance);
+      try {
+        Object.freeze(window.performance);
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
 
