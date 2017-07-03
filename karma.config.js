38f23334c4d2cfaa1752c0f8ad8317def03c89e4
Marat Dyatko <marat.dyatko@klarna.com>
2017-07-03 10:45:55 +0200

    Better reporting

--- karma.config.js
+++ karma.config.js
@@ -52,7 +52,7 @@ module.exports = function (config) {
     frameworks: ['mocha'],
     customLaunchers: customLaunchers,
     browsers: env === 'build' ? Object.keys(customLaunchers) : ['Electron'],
-    reporters: ['progress', 'saucelabs'],
+    reporters: ['mocha', 'saucelabs'],
 
     concurrency: 5,
     browserNoActivityTimeout: 30000,
@@ -77,7 +77,7 @@ module.exports = function (config) {
 
     client: {
       mocha: {
-        reporter: 'html'
+        reporter: 'mocha'
       }
     },
 
