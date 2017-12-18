f16bc1d3b611583405c79114d498c80baee2d186
Jeff Wainwright <yowainwright@gmail.com>
2017-12-18 12:03:26 -0800

    [update] moves tests => test/acceptance

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,3 @@
-
 /* eslint import/no-extraneous-dependencies: 0 */
 const babel = require('rollup-plugin-babel')
 const rollup = require('rollup')
@@ -31,61 +30,64 @@ const uglifySetup = {
       const type = comment.type
       if (type === 'comment2') return /@preserve|@license|@cc_on/i.test(text)
       return false
-    },
-  },
+    }
+  }
 }
 
-gulp.task('do-build', () => {
-  return rollup.rollup({
-    input: './src/postmate.js',
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      uglify(uglifySetup),
-    ],
-    treeshake: false,
-  }).then((bundle) => {
-    return bundle.write({
+gulp.task('do-build', () => rollup
+    .rollup({
+      input: './src/postmate.js',
+      plugins: [
+        babel({
+          exclude: 'node_modules/**'
+        }),
+        uglify(uglifySetup)
+      ],
+      treeshake: false
+    })
+    .then(bundle => bundle.write({
       file: 'build/postmate.min.js',
       format: 'umd',
       name: 'Postmate',
       banner,
-      sourcemap: false,
-    })
-  })
-})
+      sourcemap: false
+    })))
 
 gulp.task('update-readme', () => {
   const readme = path.join(__dirname, 'README.md')
   const data = fs.readFileSync(readme, 'utf-8')
   const distSize = fs.statSync(path.join(__dirname, 'build', 'postmate.min.js')).size
-  const updated = data.replace(/<span class="size">(.*?)<\/span>/,
-    `<span class="size">\`${(distSize / 1024).toFixed(1)}kb\`</span>`)
+  const updated = data.replace(
+    /<span class="size">(.*?)<\/span>/,
+    `<span class="size">\`${(distSize / 1024).toFixed(1)}kb\`</span>`
+  )
   fs.writeFileSync(readme, updated)
 })
 
 gulp.task('lint', () =>
-  gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
+  gulp
+    .src(['**/*.js', '!node_modules/**', '!build/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 )
 
 gulp.task('parent-test-server', (done) => {
-  parentServer = http.createServer(
+  parentServer = http
+    .createServer(
       connect()
         .use(serveStatic('.'))
-        .use(serveStatic('test/fixtures'))
+        .use(serveStatic('test/acceptance/fixtures'))
     )
     .listen(9000, done)
 })
 
 gulp.task('child-test-server', (done) => {
-  childServer = http.createServer(
+  childServer = http
+    .createServer(
       connect()
         .use(serveStatic('.'))
-        .use(serveStatic('test/fixtures'))
+        .use(serveStatic('test/acceptance/fixtures'))
     )
     .listen(9001, done)
 })
@@ -93,10 +95,10 @@ gulp.task('child-test-server', (done) => {
 gulp.task('do-test', () => {
   const stream = mochaPhantomJS({
     phantomjs: {
-      useColors: true,
-    },
+      useColors: true
+    }
   })
-  stream.write({ path: 'http://localhost:9001/test/runner.html' })
+  stream.write({ path: 'http://localhost:9001/test/acceptance/runner.html' })
   stream.end()
   return stream
 })
