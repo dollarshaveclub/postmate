15d2902665947e848dae3e50438d91a22cd6ed76
Jeff Wainwright <yowainwright@gmail.com>
2018-01-21 23:06:20 -0800

    [update] adds very basic tests of the interface

--- test/unit/test.postmate.js
+++ test/unit/test.postmate.js
@@ -1,3 +1,9 @@
-import postmate from '../../src/postmate'
+import Postmate from '../../src/postmate'
 
+// Jest works
 test('Jest is working', () => expect(1).toBe(1))
+// Post
+test('Postmate class is ready to rock', () => {
+  expect(typeof Postmate !== 'undefined')
+  expect(typeof Postmate).toBe('function')
+})
