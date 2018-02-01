1741ea0cb4da19159705f57e294788f656dc4a99
Jeff Wainwright <yowainwright@gmail.com>
2018-01-31 18:56:06 -0800

    [update] adds more mockinng in test.postmate.js

--- test/unit/test.postmate.js
+++ test/unit/test.postmate.js
@@ -2,8 +2,25 @@ import Postmate from '../../src/postmate'
 
 // Jest works
 test('Jest is working', () => expect(1).toBe(1))
-// Post
+
+// test API
 test('Postmate class is ready to rock', () => {
   expect(typeof Postmate !== 'undefined')
   expect(typeof Postmate).toBe('function')
+  expect(typeof Postmate.Model !== 'undefined')
+  expect(typeof Postmate.Model).toBe('function')
+  expect(typeof Postmate.Promise !== 'undefined')
+  expect(typeof Postmate.Promise).toBe('function')
+  expect(typeof Postmate.debug === false)
+})
+
+test('Postmate mocking', () => {
+  Postmate.debug = true
+  const test = new Postmate({
+    container: document.body,
+    url: 'http://child.com/',
+    model: { foo: 'bar' }
+  })
+  expect(typeof test.debug === true)
+  expect(typeof test).toBe('object')
 })
