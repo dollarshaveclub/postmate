7484519808426693b3152a9b9b601745d3f048e5
Jeff Wainwright <yowainwright@gmail.com>
2018-02-13 15:57:42 -0800

    Remove gulp (#45)
    * [init] initial update for less gulp-dep
    
    * [update] removes most gulp dependency
    
    * [update] creates scripts folder for scripts 😍

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,71 +1,12 @@
-const babel = require('rollup-plugin-babel')
-const rollup = require('rollup')
 const connect = require('connect')
-const fs = require('fs')
 const gulp = require('gulp')
 const http = require('http')
 const mochaPhantomJS = require('gulp-mocha-phantomjs')
 const path = require('path')
 const serveStatic = require('serve-static')
-const uglify = require('rollup-plugin-uglify')
 
 let parentServer 
 let childServer 
-
-const pkg = require('./package.json')
-
-const banner = `/**
-  * ${pkg.name} - ${pkg.description}
-  * @version ${pkg.version}
-  * @link ${pkg.homepage}
-  * @author ${pkg.author}
-  * @license ${pkg.license} */
-`
-const uglifySetup = {
-  output: {
-    comments(node, comment) {
-      const text = comment.value
-      const type = comment.type
-      if (type === 'comment2') return /@preserve|@license|@cc_on/i.test(text)
-      return false
-    }
-  }
-}
-
-const babelSetup = {
-  babelrc: false,
-  presets: [['es2015', { modules: false }]],
-  plugins: ["transform-class-properties"],
-  exclude: 'node_modules/**'
-}
-
-gulp.task('do-build', () => rollup
-    .rollup({
-      input: './src/index.js',
-      plugins: [
-        babel(babelSetup),
-        uglify(uglifySetup)
-      ],
-      treeshake: false
-    })
-    .then(bundle => bundle.write({
-      file: 'build/postmate.min.js',
-      format: 'umd',
-      name: 'Postmate',
-      banner,
-      sourcemap: false
-    })))
-
-gulp.task('update-readme', () => {
-  const readme = path.join(__dirname, 'README.md')
-  const data = fs.readFileSync(readme, 'utf-8')
-  const distSize = fs.statSync(path.join(__dirname, 'build', 'postmate.min.js')).size
-  const updated = data.replace(
-    /<span class="size">(.*?)<\/span>/,
-    `<span class="size">\`${(distSize / 1024).toFixed(1)}kb\`</span>`
-  )
-  fs.writeFileSync(readme, updated)
-})
 
 gulp.task('parent-test-server', (done) => {
   parentServer = http
@@ -102,7 +43,3 @@ gulp.task('test', ['parent-test-server', 'child-test-server', 'do-test'], () => 
   parentServer.close()
   childServer.close()
 })
-
-gulp.task('watch', () => gulp.watch('./src/postmate.js', ['build']))
-gulp.task('build', ['do-build', 'update-readme'])
-gulp.task('build-watch', ['build', 'watch'])
---
+++ configs/rollup.config.standard.js
@@ -0,0 +1,25 @@
+import {
+  babelSetup,
+  banner,
+  name,
+  uglifyOutput,
+  version
+} from '../configs/config'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+
+export default {
+  input: 'src/index.js',
+  plugins: [
+    babel(babelSetup),
+    uglify(uglifyOutput)
+  ],
+  treeshake: false,
+  output: {
+    banner,
+    file: 'build/postmate.min.js',
+    format: 'umd',
+    name: 'Postmate',
+    sourcemap: false
+  }
+}
