2c5664d3a0a3570290790d7a5f5712f70035aced
Jeff Wainwright <yowainwright@gmail.com>
2018-10-03 20:18:23 -0700

    [version] patch version bump w/ @lots0logs fix

--- build/postmate.dev.js
+++ build/postmate.dev.js
@@ -1,6 +1,6 @@
 /**
   postmate - A powerful, simple, promise-based postMessage library
-  @version v1.4.4
+  @version v1.4.5
   @link https://github.com/dollarshaveclub/postmate
   @author Jacob Kelley <jakie8@gmail.com>
   @license MIT
