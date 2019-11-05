1c6a92d6bfd24a3ff17d07d297080de93db57580
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-11-05 03:31:26 +0100

    Use addEventListener to attach load listener (#171)

--- src/postmate.js
+++ src/postmate.js
@@ -332,7 +332,7 @@ class Postmate {
       if (this.frame.attachEvent) {
         this.frame.attachEvent('onload', loaded)
       } else {
-        this.frame.onload = loaded
+        this.frame.addEventListener('load', loaded)
       }
 
       if (process.env.NODE_ENV !== 'production') {
